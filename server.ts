2c3b591ea5ca653be90d49c2c9658f1be0b80765
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2023-09-30 17:06:22 +0500

    fix: fixing api calls

--- server.ts
+++ server.ts
@@ -8,6 +8,9 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).json({ message: "hello" });
+});
 app.use("/user", userRouter);
 app.use("/nfts", nftRouter);
 app.use("/collection", collectionRouter);
